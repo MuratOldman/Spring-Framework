669963f2f245bc560d04024de019478a2053e735
MuratOldman <muratkara15102@gmail.com>
2022-08-28 19:59:12 -0400

    Spring 08 MVC Model

---
+++ Spring-08-MVC-Model/src/main/java/com/company/controller/StudentController.java
@@ -0,0 +1,40 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homepage(Model model){
+
+        model.addAttribute("name","MySchool");
+        model.addAttribute("course","MVC");
+
+        String subject="Thymeleaf practice";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId(0-1000) and show it in your UI
+        int studentId=new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers",numbers);
+
+
+
+        return "/student/welcome";
+    }
+
+}
