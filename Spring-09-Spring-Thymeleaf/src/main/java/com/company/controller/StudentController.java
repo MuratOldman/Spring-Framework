284db07a37baf860e4db028907b3b159c7a27714
MuratOldman <muratkara15102@gmail.com>
2022-09-14 21:27:45 -0400

    I add @GetMapping annotation

--- Spring-09-Spring-Thymeleaf/src/main/java/com/company/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/company/controller/StudentController.java
@@ -3,18 +3,22 @@ package com.company.controller;
 import com.company.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+   // @RequestMapping(value = "/register",method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("students",DataGenerator.createStudent());
         return "/student/register";
     }
-    @RequestMapping("/welcome")
+   // @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
         return "student/welcome";
     }
