7686b522c28005cdd99f4b45ca8b7d9f95972f3d
MuratOldman <muratkara15102@gmail.com>
2022-10-10 14:13:50 -0400

    I created tables through hibernate with @Entity annotation

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,18 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+   // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   // @Column(name = "studentFirstName")
+    private String firstName;
+   // @Column(name = "studentLastName")
+    private String lastName;
+    private String email;
+}
