5676d957e14b6b5e2484dff991db4201390b419a
MuratOldman <muratkara15102@gmail.com>
2022-12-03 17:19:52 -0500

    ResponseEntity<ResponseWrapper> getCourseById

--- Spring-15-REST/src/main/java/com/example/controller/CourseController_ResponseWrapper.java
+++ Spring-15-REST/src/main/java/com/example/controller/CourseController_ResponseWrapper.java
@@ -5,6 +5,7 @@ import com.example.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,11 @@ public class CourseController_ResponseWrapper {
                 .header("Version","V3")
                 .body(new ResponseWrapper("Data successfully retrieved",courseService.getCourses()));
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable Long id){
+        return  ResponseEntity
+                .ok(new ResponseWrapper("course"+ id+" retrieved",courseService.getCourseById(id)));
+    }
+
 }
