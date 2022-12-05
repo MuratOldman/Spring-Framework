d6798986bd216c4054bc956eb926dc8938cbcd20
MuratOldman <muratkara15102@gmail.com>
2022-12-04 21:10:50 -0500

    I added UserClient.java, ResponseWrapper.java, and wrote   @RestController   public class Consume_FeignClient

--- Spring-17-REST-Consuming/src/main/java/com/example/controller/Consume_FeignClient.java
+++ Spring-17-REST-Consuming/src/main/java/com/example/controller/Consume_FeignClient.java
@@ -1,4 +1,26 @@
 package com.example.controller;
 
+
+import com.example.client.UserClient;
+import com.example.dto.ResponseWrapper;
+import com.example.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class Consume_FeignClient {
+
+    private final UserClient userClient;
+
+    public Consume_FeignClient(UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    @GetMapping("/api/v2/users")
+    public ResponseEntity<ResponseWrapper> getAllUsers(){
+        return ResponseEntity.ok(new ResponseWrapper("Users are retrieved",userClient.getUsers()));
+    }
 }
